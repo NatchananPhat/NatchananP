55891fc568068245fdc13c63819d1bf526d3ca4f
NatchananP <natchanan_phat@cmu.ac.th>
2023-12-01 15:37:46 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
+    int x;
+    int y = 0;
+    int z = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    while(x != 0){
+        if(x%2 == 0){
+            y++;
+        }
+        else{
+            z++;
+        }
+        cout << "Enter an integer: ";
+        cin >> x;
+    }
+    cout << "#Even numbers = " << y << "\n";
+    cout << "#Odd numbers = " << z;
     return 0;
 }
